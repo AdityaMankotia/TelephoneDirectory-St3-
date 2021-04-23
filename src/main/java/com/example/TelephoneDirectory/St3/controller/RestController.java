5f6b7c5a63c8db4db6cea4a8e60172632b8e31fc
AdityaMankotia <adityamankotia370@gmail.com>
2021-04-23 11:32:22 +0530

    second  commit

--- src/main/java/com/example/TelephoneDirectory/St3/controller/RestController.java
+++ src/main/java/com/example/TelephoneDirectory/St3/controller/RestController.java
@@ -1,4 +1,39 @@
 package com.example.TelephoneDirectory.St3.controller;
 
+import com.example.TelephoneDirectory.St3.entity.User;
+import com.example.TelephoneDirectory.St3.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@CrossOrigin
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+    private UserService userService;
+
+    @Autowired
+    private UserController userController;
+
+
+    @RequestMapping(value = "/checkuser",method = RequestMethod.POST)
+    public String usercheck(@RequestBody String data) throws JsonProcessingException {
+        System.out.println(data);
+        User user = new ObjectMapper().readValue(data, User.class);
+        System.out.println(user.toString());
+        if(userService.checkuser(user)) {
+            System.out.println("Userfound");
+            return "";
+        }
+        else
+        {
+
+            return "false";
+        }
+
+
+    }
 }
--- src/main/java/com/example/TelephoneDirectory/St3/controller/UserController.java
+++ src/main/java/com/example/TelephoneDirectory/St3/controller/UserController.java
@@ -1,4 +1,51 @@
 package com.example.TelephoneDirectory.St3.controller;
 
+import com.example.TelephoneDirectory.St3.entity.User;
+import com.example.TelephoneDirectory.St3.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+    @Autowired
+    public UserService userService;
+
+    @RequestMapping("/")
+    public String home(){
+
+        return "index";
+    }
+
+    @RequestMapping("/Login")
+    public String login(){
+
+        return "/html/Login";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(){
+
+        return "/html/signup";
+    }
+
+    @RequestMapping("/dashboard")
+    public String usercheck(@RequestBody String data) throws JsonProcessingException {
+        System.out.println(data);
+        User user = new ObjectMapper().readValue(data, User.class);
+        System.out.println(user.toString());
+        if(userService.checkuser(user)) {
+            System.out.println("Userfound");
+            return "/html/Dashboard";
+        }
+        else
+        {
+            return "/html/Login";
+        }
+
+
+    }
 }
